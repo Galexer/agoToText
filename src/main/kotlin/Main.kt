762b88b96c5b99af7e4e302db0e90ea754ba8f8e
Galina Ry <galya.rybynok@gmail.com>
2022-08-02 10:36:54 +0300

    fun agoToText in rus

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,46 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package ru.netology
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val lastTimeOnlineInSec = 800
+    println(agoToText(lastTimeOnlineInSec))
+}
+
+fun agoToText(lastTimeOnline : Int): String {
+    val sec60 = 60
+    val hour = 60 * 60
+    val day = hour * 24
+    val toMin = lastTimeOnline / sec60
+    val toHours = lastTimeOnline / hour
+
+    return when(lastTimeOnline) {
+        in 0..sec60 -> "был(а) только что"
+        in sec60 + 1 .. hour -> "был(а) в сети $toMin ${minMaker(toMin)} назад"
+        in hour + 1 .. day -> "был(а) в сети $toHours ${hourMaker(toHours)} назад"
+        in day + 1 .. 2 * day -> "был(а) сегодня"
+        in 2 * day + 1 .. 3 * day -> "был(а) вчера"
+        else -> "был(а) давно"
+    }
+}
+
+fun minMaker(min : Int) : String {
+    return when(min){
+        1 -> "минута"
+        in 2 .. 4 -> "минуты"
+        in 5 .. 19 -> "минут"
+        else -> {
+            when (min.toString()[min.toString().length - 1]) {
+                '1' -> "минута"
+                in '2'..'4' -> "минуты"
+                else -> "минут"
+            }
+        }
+    }
+}
+
+fun hourMaker(hour : Int) : String {
+    return when(hour) {
+        1, 21 -> "час"
+        in 2..5, in 22..24 -> "часа"
+        else -> "часов"
+    }
 }
